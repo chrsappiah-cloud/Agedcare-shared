41d7bfbf2d6272e2e2f03415385e6f228d50d5d3
Christopher Appiah-Thompson <chrsappiah@gmail.com>
2026-05-10 08:42:53 +1000

    Initial Commit

---
+++ Agedcare-sharedTests/Agedcare_sharedTests.swift
@@ -0,0 +1,19 @@
+//
+//  Agedcare_sharedTests.swift
+//  Agedcare-sharedTests
+//
+//  Created by Christopher Appiah-Thompson  on 10/5/2026.
+//
+
+import Testing
+@testable import Agedcare_shared
+
+struct Agedcare_sharedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
